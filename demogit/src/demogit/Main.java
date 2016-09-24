a0d25d2bcb2522d54e7e14e8ccbf7bb76c3bef20
Javier <Javier@Javier-PC>
2016-09-24 19:46:45 -0300

    agrega funciones a vehiculo

--- demogit/src/demogit/Main.java
+++ demogit/src/demogit/Main.java
@@ -3,6 +3,7 @@ package demogit;
 public class Main {
 	public static void main(String args[]){
 		System.out.println(greet("Javier"));
+		Vehicle miauto = new Vehicle();
 	}
 	
 	private static String greet(String name){
--- demogit/src/demogit/Vehicle.java
+++ demogit/src/demogit/Vehicle.java
@@ -2,5 +2,14 @@ package demogit;
 
 public class Vehicle {
 	private String model;
+	private int key;
 	private String brand;
+	
+	public Vehicle(){}
+		
+	public void turnon(int key){
+		if(this.key==key){
+			System.out.print("Encendido");
+		}
+	}
 }
